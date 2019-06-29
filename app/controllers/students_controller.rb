4e8fdb3fc905f9eace2990665fbe3c35f65d2f19
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-06-29 18:58:40 -0400

    has intended behaviors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,19 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(first_name: params[:student][:first_name])
+    @student.update(last_name: params[:student][:last_name])
+    @student.update(active: params[:student][:active])
+    redirect_to "/students/#{params[:id]}"
   end
 
   private
